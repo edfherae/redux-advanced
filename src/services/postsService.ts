6247f6fe0d395379ed9819f450a183281ece1c69
edfherae <163173239+edfherae@users.noreply.github.com>
2026-09-08 13:47:47 +0300

    Удалил зависимости @types/react-redux^7.1.34 и redux-thunk^3.1.0, обновил TS до 5.6.2. Починилось добавление middleware и определение типа AppDispatch

---
+++ src/services/postsService.ts
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const postsAPI = createApi({
+  reducerPath: "postsAPI",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com/",
+  }),
+  endpoints: (build) => ({
+    fetchAllPosts: build.query({
+      query: () => ({
+        url: "users",
+      }),
+    }),
+  }),
+});
--- src/store/store.ts
+++ src/store/store.ts
@@ -5,18 +5,21 @@ import {
   type UnknownAction,
 } from "@reduxjs/toolkit";
 import userReducer from "./reducers/usersSlice";
+import { postsAPI } from "../services/postsService";
 
 const rootReducer = combineReducers({
   users: userReducer,
+  [postsAPI.reducerPath]: postsAPI.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat(postsAPI.middleware),
   });
 };
 
 export type AppState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
-// export type AppDispatch = AppStore["dispatch"];
-export type AppDispatch = ThunkDispatch<AppState, unknown, UnknownAction>;
+export type AppDispatch = AppStore["dispatch"];
